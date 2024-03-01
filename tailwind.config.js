312b9ab93646343552f7665d82c03b074dc9aa5b
Daridjcm <daricorreamedina@gmail.com>
2024-02-29 20:44:53 -0500

    Add @egoist/tailwindcss-icons plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 const { addDynamicIconSelectors } = require('@iconify/tailwind');
+const { iconsPlugin, dynamicIconsPlugin } = require('@egoist/tailwindcss-icons');
 
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [addDynamicIconSelectors()],
+  plugins: [addDynamicIconSelectors(),iconsPlugin(), dynamicIconsPlugin()],
 }
